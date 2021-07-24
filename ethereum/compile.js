c45068c58d5f0ba1cb101d85ac6a9be179b84a5a
Suranjan <suranjanbiswas2002@gmail.com>
2021-07-24 18:37:07 +0530

    update: updated the compile logic when creating a new file after compilation

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -13,7 +13,7 @@ fs.ensureDirSync(buildPath);
 
 for (let contract in output) {
   fs.outputJsonSync(
-    path.resolve(buildPath, contract + ".json"),
+    path.resolve(buildPath, `${contract.substring(1)}.json`),
     output[contract]
   );
 }
