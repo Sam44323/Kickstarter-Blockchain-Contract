d37a9e872753da4cdf62d81558e6734d41678217
Suranjan <suranjanbiswas2002@gmail.com>
2021-07-25 23:47:20 +0530

    update: updated the env file

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const Web3 = require("web3");
-const { interface, bytecode } = require("./compile");
+const compiledFactory = require("./build/CampaignFactory.json");
 
 const provider = new HDWalletProvider(
   process.env.SECRET_PHRASE,
@@ -12,16 +12,17 @@ const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-  const result = await new web3.eth.Contract(JSON.parse(interface))
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
     .deploy({
-      data: `0x${bytecode}`,
+      data: `0x${compiledFactory.bytecode}`,
     })
     .send({
       from: accounts[0],
       gas: "1000000",
       gasPrice: web3.utils.toWei("2", "gwei"),
     });
-  console.log(interface);
   console.log("Contract deployed to ", result.options.address);
 };
 
