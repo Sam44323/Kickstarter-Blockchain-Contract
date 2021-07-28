55253d87dbc5f6bdf5af6f73f96c98f7c2182e03
Suranjan <suranjanbiswas2002@gmail.com>
2021-07-28 22:55:31 +0530

    add: added _app.tsx file for containering the data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import factory from "../ethereum/Factory";
 
-const App: React.FC = () => {
+const CampaignHome: React.FC = () => {
+  useEffect(() => {
+    const getCampaignsList = async () => {
+      const campaigns = await factory.methods.getDeployedCampaigns().call();
+      console.log(campaigns);
+    };
+    getCampaignsList();
+  });
   return <>nice</>;
 };
 
-export default App;
+export default CampaignHome;
