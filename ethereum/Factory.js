841528e269e0220768cc4c06f2bde4cbbca34770
Suranjan <suranjanbiswas2002@gmail.com>
2021-07-28 22:09:47 +0530

    add: added a Factory.js file to create an instance for the deployed contract for factory

---
+++ ethereum/Factory.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+import web3 from "./web3";
+import CampaignFactory from "./build/CampaignFactory.json";
+
+const instance = new web3.eth.Contract(
+  JSON.parse(CampaignFactory.interface),
+  process.env.DEPLOYED_FACTORY
+);
+
+export default instance;
