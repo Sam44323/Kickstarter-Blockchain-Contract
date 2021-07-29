248c99de6dafe9b698a4f4a5a83d1219f41deffc
Suranjan <suranjanbiswas2002@gmail.com>
2021-07-29 14:14:11 +0530

    update: updated the web3.js file

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -4,12 +4,14 @@ let web3;
 
 if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
   // We are in the browser and metamask is running.
+
   window.ethereum.request({ method: "eth_requestAccounts" });
-  web3 = new Web3(window.ethereum);
+  web3 = new Web3(window.ethereum); // using the same provider as the browser
 } else {
   // We are on the server *OR* the user is not running metamask
+
   const provider = new Web3.providers.HttpProvider(process.env.URL);
-  web3 = new Web3(provider);
+  web3 = new Web3(provider); // using our custom provider provided by infura for rinkeby
 }
 
 export default web3;
