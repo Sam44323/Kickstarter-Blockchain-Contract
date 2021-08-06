d99033fd28c3b0d1fa044503236c20bb23f21183
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-06 23:02:00 +0530

    add: added a Header.tsx file for layout

---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+
+const Header: React.FC = () => {
+  return <></>;
+};
+
+export default Header;
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "./Header";
 
 const Layout: React.FC = ({ children }) => {
-  return <>{children}</>;
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
 };
 
 export default Layout;
