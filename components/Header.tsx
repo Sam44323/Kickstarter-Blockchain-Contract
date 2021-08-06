f6e4262c5a5e606bd792df4dfdc24d86557715eb
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-06 23:08:33 +0530

    add: added the basic layout for the Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,34 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
 
+const MenuItem: React.FC<{ name: string; clickHandler: () => void }> = ({
+  name,
+  children,
+  clickHandler,
+}) => {
+  return (
+    <Menu.Item name={name} onClick={clickHandler}>
+      {children}
+    </Menu.Item>
+  );
+};
+
 const Header: React.FC = () => {
-  return <></>;
+  return (
+    <Menu>
+      <MenuItem name="crowdcoin" clickHandler={() => null}>
+        CrowdCoin
+      </MenuItem>
+      <Menu.Menu position="right">
+        <MenuItem name="campaigns" clickHandler={() => null}>
+          Campaigns
+        </MenuItem>
+        <MenuItem name="add" clickHandler={() => null}>
+          +
+        </MenuItem>
+      </Menu.Menu>
+    </Menu>
+  );
 };
 
 export default Header;
