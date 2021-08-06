adca2637725bd78acd6e299349bc2f97896d9418
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-06 22:13:10 +0530

    add: added the semantic-ui css file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { AppProps } from "next/app";
+import "semantic-ui-css/semantic.min.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
