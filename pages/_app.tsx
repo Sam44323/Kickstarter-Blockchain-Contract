2c2605bfc03d311e23708fe87184bcd564652d12
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-06 22:44:53 +0530

    add: added the Header component to main layout file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { AppProps } from "next/app";
 import "semantic-ui-css/semantic.min.css";
+import Header from "../components/Header";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
