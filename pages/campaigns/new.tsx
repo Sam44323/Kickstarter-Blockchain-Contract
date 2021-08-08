32bdea0f7736913ec04803b1b7e4f9bad1e989ed
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-08 22:53:35 +0530

    add: added an error handler for campaign-creation

--- pages/campaigns/new.tsx
+++ pages/campaigns/new.tsx
@@ -1,22 +1,44 @@
 import React, { FormEvent, useState } from "react";
-import { Button, Form, Container, Input } from "semantic-ui-react";
+import { Button, Form, Container, Input, Message } from "semantic-ui-react";
 import factory from "../../ethereum/Factory";
 import web3 from "../../ethereum/web3";
 
 const NewCampaign: React.FC = () => {
   const [price, setPrice] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
+
+  const disappearingError = () => {
+    setTimeout(() => {
+      setError(false);
+    }, 3500);
+  };
 
   const createNewCampaign = async (e: FormEvent) => {
     e.preventDefault();
 
-    const accounts = await web3.eth.getAccounts()[0]; // getting the accounts
-    await factory.methods.createCampaign().send({
-      from: accounts[0],
-    });
+    try {
+      const accounts = await web3.eth.getAccounts(); // getting the accounts
+      await factory.methods.createCampaign(price).send({
+        from: accounts[0],
+      });
+    } catch (err) {
+      console.log(err);
+      setError(true);
+      disappearingError();
+    }
   };
 
   return (
     <Container fluid={true}>
+      {error && (
+        <Message negative>
+          <Message.Header>Transaction Failed!</Message.Header>
+          <p>
+            Either you sent value less than min contribution, or sent some wrong
+            value!
+          </p>
+        </Message>
+      )}
       <h3>Create a new campaign</h3>
       <Form>
         <Form.Field>
@@ -24,7 +46,7 @@ const NewCampaign: React.FC = () => {
           <Input
             label="wei"
             size="big"
-            placeholder="0"
+            placeholder="Enter amount!"
             labelPosition="right"
             onChange={(e) => setPrice(e.target.value)}
             value={price}
