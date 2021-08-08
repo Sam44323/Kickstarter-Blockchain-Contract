8e2868956f061421f9eaef7dc50338ca4f334033
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-09 00:21:08 +0530

    update: updated the configuration for deploy.js

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,17 +1,20 @@
-require("dotenv").config();
+const path = require("path");
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const Web3 = require("web3");
 const compiledFactory = require("./build/CampaignFactory.json");
+require("dotenv").config({
+  path: path.resolve(__dirname, ".env"),
+});
 
 const provider = new HDWalletProvider(
   process.env.SECRET_PHRASE,
-  "https://rinkeby.infura.io/v3/c65de9dd9e6a4d2cb17c756fb3f6a446"
+  "https://rinkeby.infura.io/v3/e904562b4f2146c3850485a7ecd87240"
 );
 
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-  await web3.eth.getAccounts();
+  const accounts = await web3.eth.getAccounts();
   await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
     .deploy({
       data: compiledFactory.bytecode,
