c5613a134d31043d1ef5ffee972e09753adf6dbf
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-08 20:57:31 +0530

    add: added a form for creating a new campaign

--- pages/campaigns/new.tsx
+++ pages/campaigns/new.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Button, Form, Container } from "semantic-ui-react";
 
 const NewCampaign: React.FC = () => {
-  return <h1>New</h1>;
+  return (
+    <Container fluid={true}>
+      <h3>Create a new campaign</h3>
+      <Form>
+        <Form.Field>
+          <label>Minimum Contribution (wei)</label>
+          <input
+            type="text"
+            placeholder="0"
+            style={{
+              width: "40%",
+            }}
+          />
+        </Form.Field>
+        <Button type="submit" secondary icon="add" content="Create" />
+      </Form>
+    </Container>
+  );
 };
 
 export default NewCampaign;
