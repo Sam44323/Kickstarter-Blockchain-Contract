f82b1c718702721d4b0c64f5cc852462c1d661e4
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-08 21:36:10 +0530

    add: added change handler to the input

--- pages/campaigns/new.tsx
+++ pages/campaigns/new.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, Container, Input } from "semantic-ui-react";
 
 const NewCampaign: React.FC = () => {
+  const [price, setPrice] = useState<string>("");
+
   return (
     <Container fluid={true}>
       <h3>Create a new campaign</h3>
       <Form>
         <Form.Field>
           <label>Minimum Contribution</label>
-          <Input label="wei" size="big" placeholder="0" labelPosition="right" />
+          <Input
+            label="wei"
+            size="big"
+            placeholder="0"
+            labelPosition="right"
+            onChange={(e) => setPrice(e.target.value)}
+            value={price}
+          />
         </Form.Field>
         <Button type="submit" secondary icon="add" content="Create" />
       </Form>
