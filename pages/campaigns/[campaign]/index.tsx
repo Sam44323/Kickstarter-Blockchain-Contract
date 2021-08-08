6cacc04d0b47da7ca6016639ff0499ebf5f1eff7
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-09 00:45:35 +0530

    add: added query params extracter

---
+++ pages/campaigns/[campaign]/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+const Campaign: React.FC = () => {
+  const { query } = useRouter();
+  return <>nice</>;
+};
+
+export default Campaign;
