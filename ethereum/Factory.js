9180b5e641e6a2f8a9d57abf53d538808d047dae
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-10 11:31:13 +0530

    update: updated the contract

--- ethereum/Factory.js
+++ ethereum/Factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0xfc562412a06f2fD85eeC406F74C6620c0AA2D49D"
+  "0x590dCE7b4b09Eb8d1960767Ed9515E6336b20416"
 );
 
 export default instance;
