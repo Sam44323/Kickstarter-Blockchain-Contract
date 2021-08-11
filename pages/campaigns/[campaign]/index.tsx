87b5f789b91518c159e24b1540c8b210b10f281b
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-11 21:48:48 +0530

    add: added card containers for rendering summary for campaigns

--- pages/campaigns/[campaign]/index.tsx
+++ pages/campaigns/[campaign]/index.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { Container } from "semantic-ui-react";
+import { Card, Container, Grid, GridColumn } from "semantic-ui-react";
 import Head from "next/head";
 import { GetServerSideProps } from "next";
 import CampaignGenerator from "../../../ethereum/Campaign";
 
+const ColumnComponent: React.FC<{ header: string; desc: string }> = ({
+  header,
+  desc,
+}) => {
+  return (
+    <GridColumn>
+      <Card>
+        <Card.Content header={header} description={desc} />
+      </Card>
+    </GridColumn>
+  );
+};
+
 const Campaign: React.FC<{ data: any }> = ({ data }) => {
   const { query } = useRouter();
-  console.log(JSON.parse(data));
+  data = JSON.parse(data);
   return (
     <Container>
       <Head>
@@ -15,6 +28,18 @@ const Campaign: React.FC<{ data: any }> = ({ data }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <h1>Campaign Details</h1>
+      <Container>
+        <Grid columns={4} stackable>
+          <Grid.Row>
+            <ColumnComponent header="Minimum Contribution" desc={data["0"]} />
+            <ColumnComponent header="Available Balance" desc={data["1"]} />
+          </Grid.Row>
+          <Grid.Row>
+            <ColumnComponent header="Requests Count" desc={data["2"]} />
+            <ColumnComponent header="Approvers" desc={data["3"]} />
+          </Grid.Row>
+        </Grid>
+      </Container>
     </Container>
   );
 };
