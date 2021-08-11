8eb4b70f962b618646eeac8d84f9f75f7d1c04af
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-11 21:28:47 +0530

    add: added docs for Campaign.js file

--- ethereum/Campaign.js
+++ ethereum/Campaign.js
@@ -1,6 +1,8 @@
 import web3 from "./web3";
 import Campaign from "./build/Campaign.json";
 
+// returning a particular campaign based on the address
+
 const getCampaign = async (address) => {
   const instance = new web3.eth.Contract(
     JSON.parse(Campaign.interface),
