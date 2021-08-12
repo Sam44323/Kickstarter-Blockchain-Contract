2bef0d5fbf1ea76e160849839778e4d611904832
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-12 21:54:38 +0530

    add: added CampaignGenerator function for getting the campaign based on the address from query

--- pages/campaigns/[campaign]/requests/index.tsx
+++ pages/campaigns/[campaign]/requests/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useRouter } from "next/router";
+import { GetServerSideProps } from "next";
 import { Container, Button } from "semantic-ui-react";
+import CampaignGenerator from "../../../../ethereum/Campaign";
 
 const Request: React.FC = () => {
   const { query, push } = useRouter();
@@ -14,6 +16,16 @@ const Request: React.FC = () => {
       />
     </Container>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const campaign = await CampaignGenerator(context.query.campaign);
+
+  return {
+    props: {
+      data: "",
+    },
+  };
 };
 
 export default Request;
