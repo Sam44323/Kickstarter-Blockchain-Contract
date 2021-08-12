4f583f5d309334d5c78479cd989653b4d6435bcb
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-12 21:12:08 +0530

    add: added container for index file for requestas

--- pages/campaigns/[campaign]/requests/index.tsx
+++ pages/campaigns/[campaign]/requests/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useRouter } from "next/router";
+import { Container } from "semantic-ui-react";
 
 const Request: React.FC = () => {
   const { query } = useRouter();
-  return <>{query.address}</>;
+  return <Container>{query.campaign}</Container>;
 };
 
 export default Request;
