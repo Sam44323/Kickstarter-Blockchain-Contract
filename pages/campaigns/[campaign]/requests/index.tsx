d9ffb28da2e1ca3171a12fba4b00c3657d4ea6c9
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-12 21:28:18 +0530

    add: added a page component for creating a new request

--- pages/campaigns/[campaign]/requests/index.tsx
+++ pages/campaigns/[campaign]/requests/index.tsx
@@ -3,11 +3,15 @@ import { useRouter } from "next/router";
 import { Container, Button } from "semantic-ui-react";
 
 const Request: React.FC = () => {
-  const { query } = useRouter();
+  const { query, push } = useRouter();
   return (
     <Container>
       <h3>Requests</h3>
-      <Button primary content="Add Requests" />
+      <Button
+        primary
+        content="Add Requests"
+        onClick={() => push(`/campaigns/${query.campaign}/requests/new`)}
+      />
     </Container>
   );
 };
---
+++ pages/campaigns/[campaign]/requests/new.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+const NewRequest: React.FC = () => {
+  return (
+    <Container>
+      <h1>New request</h1>
+    </Container>
+  );
+};
+
+export default NewRequest;
