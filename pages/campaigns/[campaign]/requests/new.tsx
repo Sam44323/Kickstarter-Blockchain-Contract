d58ae8de4bc714605479a03347207c444c59d2a0
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-13 22:22:32 +0530

    add: added contract interaction for creating a new request

--- pages/campaigns/[campaign]/requests/new.tsx
+++ pages/campaigns/[campaign]/requests/new.tsx
@@ -10,6 +10,7 @@ import {
 import { NewRequestInput, RequestForm } from "../../../../utils/interfaces";
 import CampaignCreator from "../../../../ethereum/Campaign";
 import { useRouter } from "next/router";
+import web3 from "../../../../ethereum/web3";
 
 const FormInputContainer: React.FC<NewRequestInput> = (props) => {
   return (
@@ -36,12 +37,12 @@ const NewRequest: React.FC<{ methods: any }> = ({ methods }) => {
   const [campaign, setCampaign] = React.useState<any>();
   const [error, setError] = React.useState<boolean>(false);
   const [loading, setLoading] = React.useState<boolean>(false);
-  const { query } = useRouter();
+  const { query, push } = useRouter();
 
   React.useEffect(() => {
     const getContract = async () => {
       const campaignData = await CampaignCreator(query.campaign);
-      setCampaign(campaignData.methods);
+      setCampaign(campaignData);
     };
     getContract();
   }, []);
@@ -53,15 +54,34 @@ const NewRequest: React.FC<{ methods: any }> = ({ methods }) => {
     }));
   };
 
-  const formSubmitHandler = (e: FormEvent) => {
-    e.preventDefault();
-    setRequestForm({
-      amount: "",
-      description: "",
-      recipient: "",
-    });
+  const disappearingError = () => {
+    setTimeout(() => {
+      setError(false);
+    }, 7500);
   };
 
+  const formSubmitHandler = async (e: FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const { description, amount, recipient } = requestForm;
+      const accounts = await web3.eth.getAccounts();
+      await campaign.methods
+        .createRequest(
+          description,
+          web3.utils.toWei(amount, "ether"),
+          recipient
+        )
+        .send({
+          from: accounts[0],
+        });
+      push("/");
+    } catch (error) {
+      setError(true);
+      setLoading(false);
+      disappearingError();
+    }
+  };
   return (
     <>
       {loading && <Loader active size="big" content="Processing" />}
@@ -78,7 +98,7 @@ const NewRequest: React.FC<{ methods: any }> = ({ methods }) => {
           />
         )}
         <h1>Create a New Request</h1>
-        <Form onSubmit={formSubmitHandler}>
+        <Form onSubmit={(e) => formSubmitHandler(e)}>
           <FormInputContainer
             label="Description"
             changeHandler={valueChanger}
