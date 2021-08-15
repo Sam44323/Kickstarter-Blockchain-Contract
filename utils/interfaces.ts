907991570ce5dff6bd89681979429f6df00852a4
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-15 22:28:57 +0530

    add: added iinterface for CustomRow component

--- utils/interfaces.ts
+++ utils/interfaces.ts
@@ -11,3 +11,12 @@ export interface RequestForm {
   amount: string;
   recipient: string;
 }
+
+export interface CustomRowInter {
+  id: string;
+  approvalCount: string;
+  complete: boolean;
+  description: string;
+  recipient: string;
+  value: string;
+}
